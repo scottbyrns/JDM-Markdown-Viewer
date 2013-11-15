8cb9208575e47dcdf02dfb8d32ebaae64d5ce8e7
Scott Byrns <scott@scotts-MacBook-Pro.local>
2013-11-15 07:53:20 -0700

    Initial implementation.

--- src/MarkdownViewer.js
+++ src/MarkdownViewer.js
@@ -1,6 +1,21 @@
-com.scottbyrns.widgets.jdm.MarkdownViewer.MarkdownViewer = function () {
-
-};
-
-
-com.scottbyrns.widgets.jdm.MarkdownViewer.MarkdownViewer.prototype = {};
+LiveWidgets.addWidget({
+	name: "markdown-viewer",
+	model: {},
+	controller: {
+	
+		handleMessage: function (message, channel, id) {
+			if (channel == "render-markdown") {
+				
+				this.element.innerHTML = markdown.toHTML(message);
+				
+			}
+		}
+		
+	},
+	constructor: function () {
+		
+	},
+	reinit: function () {
+		
+	}
+})
